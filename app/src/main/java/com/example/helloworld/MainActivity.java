979db06add16a9aec35e4ed7b98b32993e782790
ellyciasetiadi <e.setiadi@student.unsw.edu.au>
2020-02-26 12:06:16 +1100

    update

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.helloworld;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button launchDetailActivity = findViewById(R.id.launchbutton);
+
+        launchDetailActivity = new Button(this);
+
+        launchDetailActivity.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+                LaunchDetailActivityClicked();
+            }
+
+        });
+
     }
+
+    public void LaunchDetailActivityClicked() {
+        Intent intent = new Intent(this, DetailActivity.class);
+        startActivity(intent);
+    }
+
 }
+
